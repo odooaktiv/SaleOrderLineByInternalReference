145aa23a4430c7cca612740f32252e451107f665
Odoo Aktiv <solankiriddhir@yahoo.in>
2018-09-10 12:31:52 +0530

    [IMP] license in manifest for branch 10.0

--- sale_order_line_by_internal_reference/__manifest__.py
+++ sale_order_line_by_internal_reference/__manifest__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-# Part of AktivSoftware See LICENSE file for full copyright
-# and licensing details.
+
 {
     'name': "Sale Order Lines by Internal Reference",
     'summary': """
